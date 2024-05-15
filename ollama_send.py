484b67cbcd212d5a80b46f136b88c7031fa11b06
jknbis <110455201+jknbis@users.noreply.github.com>
2024-05-15 23:33:44 +0300

    Update ollama_send.py

--- ollama_send.py
+++ ollama_send.py
@@ -1,12 +1,13 @@
-import requests
+import aiohttp
+import asyncio
 import json
 
 class OllamaSender:
     def __init__(self, model="llama3"):
-        self. system_prompt = """Review the pull request titled "[Title]" which aims to [Objective]. Assess code quality, readability, performance, and security. Check the documentation and test coverage to ensure they are thorough. Provide constructive feedback on alignment with project goals, suggesting improvements and optimizations where needed, all in a supportive and positive tone."""
+        self.system_prompt = """Review the pull request diff. Assess code quality, readability, performance, and security. Check the documentation and test coverage to ensure they are thorough. Provide constructive feedback on alignment with project goals, suggesting improvements and optimizations where needed, all in a supportive and positive tone. be short and concise."""
         self.model = model
 
-    def send_to_ollama(self, diff):
+    async def send_to_ollama(self, diff):
         headers = {
             "Content-Type": "application/json"
         }
@@ -17,22 +18,17 @@ class OllamaSender:
             "temperature": 0.5   
         }
         try:
-            response = requests.post("http://localhost:11434/api/generate", headers=headers, json=data)
-            if response.status_code == 200:
-                response_text = response.text
-                data = json.loads(response_text)
-                actual_response = (data["response"])
-                return actual_response
-            else:
-                print("Error:", response.text)
-                raise Exception('Failed to get a valid response from the model.')
+            async with aiohttp.ClientSession() as session:
+                async with session.post("http://localhost:8081/api/generate", headers=headers, json=data) as response:
+                    if response.status == 200:
+                        data = await response.json()
+                        actual_response = (data["response"])
+                        print(actual_response)
+                        return actual_response
+                    else:
+                        text = await response.text()
+                        print("Error:", text)
+                        raise Exception('Failed to get a valid response from the model.')
         except Exception as e:
             print(e)
             return None
-
-
-
-
-
-
-    
